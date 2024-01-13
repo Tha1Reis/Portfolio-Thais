4b216cbd3aa6d069e08b2ed6b5f2e11caae5c24a
Thais Reis <thaisreisbueno@hotmail.com>
2024-01-13 02:20:00 -0300

    corrigindo form

--- formulario-email.php
+++ formulario-email.php
@@ -1,6 +1,5 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recupera os dados do formulário
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $assunto = $_POST["assunto"];
@@ -10,22 +9,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $destinatario = "thaisreisbueno@hotmail.com";
 
     // Assunto do e-mail
-    $assunto = "Nova mensagem do formulário de contato";
+    $assunto_email = "Novo formulário de contato: $assunto";
 
-    // Constrói a mensagem do e-mail
+    // Construa a mensagem do e-mail
     $mensagem_email = "Nome: $nome\n";
     $mensagem_email .= "E-mail: $email\n";
     $mensagem_email .= "Assunto: $assunto\n";
     $mensagem_email .= "Mensagem:\n$mensagem";
 
-    // Envia o e-mail
-    mail($destinatario, $assunto, $mensagem_email);
+    // Enviar e-mail
+    mail($destinatario, $assunto_email, $mensagem_email);
 
-    // Exibe uma mensagem de sucesso
+    // Exibe uma mensagem de sucesso ou redireciona para uma página de agradecimento
     echo "<h2>Mensagem enviada com sucesso!</h2>";
 } else {
     // Se o formulário não foi enviado corretamente, redirecione para a página do formulário
-    header("Location: https://tha1reis.github.io/Portfolio-Thais/");
+    header("Location: seu_formulario.html");
     exit();
 }
 ?>
